37c8eb9383c9d03bc6419a521a3617e5b0eb394a
MinHieu2006 <luongminhhieu303@gmail.com>
2022-05-27 00:21:03 +0700

    part 2

--- app/src/main/java/com/example/depresol/RegisterActivity.java
+++ app/src/main/java/com/example/depresol/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.example.depresol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,4 +16,18 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void changeStatusBarColor(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+        }
+    }
+
+    public void onLoginClick(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
+    }
+
 }
