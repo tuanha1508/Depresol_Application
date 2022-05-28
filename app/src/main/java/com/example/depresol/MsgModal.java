7f526ab2fd580bdfb8a8b4f286404861d63d4b7c
tuanhaHAT <101470602+tuanhaHAT@users.noreply.github.com>
2022-05-29 01:54:37 +0700

    Update

---
+++ app/src/main/java/com/example/depresol/MsgModal.java
@@ -0,0 +1,12 @@
+package com.example.depresol;
+
+public class MsgModal {
+    private String cnt;
+
+    public MsgModal(String cnt) {this.cnt = cnt;}
+
+    public String getCnt() {return cnt;}
+
+    public void setCnt(String cnt) {this.cnt = cnt;}
+
+}
