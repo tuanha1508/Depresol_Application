096b68c1ddd54298e2f6c182ae60673f0da8042c
tuanhaHAT <101470602+tuanhaHAT@users.noreply.github.com>
2022-06-09 14:04:56 +0700

    Update

---
+++ app/src/main/java/com/example/depresol/FindItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.depresol;
+
+import android.widget.ImageView;
+
+import com.example.depresol.FindCard;
+
+public interface FindItemClickListener {
+
+    void onDashboardFindClick(FindCard findCard, ImageView imageView);
+
+}
--- app/src/main/java/com/example/depresol/FindTopicsViewpapers.java
+++ app/src/main/java/com/example/depresol/FindTopicsViewpapers.java
@@ -34,12 +34,7 @@ public class FindTopicsViewpapers extends RecyclerView.Adapter<FindTopicsViewpap
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View view = mInflater.inflate(R.layout.item_pager_card, parent, false);
-//        return new ViewHolder(view);
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-//        View v = inflater.inflate(R.layout.item_shop_card, parent, false);
-//        return new ViewHolder(v);
-
         ItemPaperCardBinding itemPagerCardBinding = ItemPaperCardBinding.inflate(inflater, parent, false);
         return new ViewHolder(itemPagerCardBinding);
     }
@@ -47,7 +42,6 @@ public class FindTopicsViewpapers extends RecyclerView.Adapter<FindTopicsViewpap
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.setBind(mFindList.get(position));
-
         holder.binding.cardViewFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,11 +69,8 @@ public class FindTopicsViewpapers extends RecyclerView.Adapter<FindTopicsViewpap
 
             binding.tvTitulo.setText(matchCourse.getName());
             binding.tvCantidadCursos.setText(matchCourse.getNumberOfCourses());
-
             Glide.with(itemView.getContext())
                     .load(matchCourse.getImageResource())
-//                .transform(new CenterCrop(), new RoundedCorners(24))
-//                .transform(new RoundedCorners(40))
                     .transform(new CenterCrop())
                     .into(binding.image);
         }
