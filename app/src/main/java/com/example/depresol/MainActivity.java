4790ee10edeada7630baf425b87c028be75d1be9
tuanhaHAT <101470602+tuanhaHAT@users.noreply.github.com>
2022-06-09 14:04:56 +0700

    Update

--- app/src/main/java/com/example/depresol/MainActivity.java
+++ app/src/main/java/com/example/depresol/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         try{
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_chat);
         }catch (Exception ex){
             Log.e("error" , ex.toString());
         }
