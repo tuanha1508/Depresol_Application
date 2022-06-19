e1601cb0dab1f0a61353b24589957750d08cac01
tuanhaHAT <101470602+tuanhaHAT@users.noreply.github.com>
2022-06-20 00:38:03 +0700

    Revert "Update"

--- app/src/main/java/com/example/depresol/MainActivity_Menu.java
+++ app/src/main/java/com/example/depresol/MainActivity_Menu.java
@@ -66,7 +66,13 @@ public class MainActivity_Menu extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainMenuBarBinding.inflate(getLayoutInflater());
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         View view = binding.getRoot();
         setContentView(view);
 
